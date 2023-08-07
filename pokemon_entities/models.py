42eff11cfc781dbb17c2233629058b781d8b23af
DeFchanel <georgyasr@mail.ru>
2023-08-07 19:19:01 +0300

    show pokemons on mainpage

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,8 +1,10 @@
 from django.db import models  # noqa F401
 import datetime
 class Pokemon(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
-    image = models.ImageField(null=True)
+    image = models.ImageField(null=True, blank=True)
+    
 
     def __str__(self):
         return '{}'.format(self.title)
