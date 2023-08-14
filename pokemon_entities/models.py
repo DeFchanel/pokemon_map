311f61d4f6b82fef3a0a790c76dc24f9468cf492
DeFchanel <georgyasr@mail.ru>
2023-08-14 19:48:24 +0300

    Add Pokemon description

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,6 +4,7 @@ class Pokemon(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
     image = models.ImageField(null=True, blank=True)
+    description = models.TextField(blank=True)
     
 
     def __str__(self):
